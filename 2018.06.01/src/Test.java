1a25c3b4bbd27e129242460e278e12e403002cc7
USER <USER@INET-26>
2018-06-01 11:45:46 +0900

    123456

--- 2018.06.01/src/Test.java
+++ 2018.06.01/src/Test.java
@@ -1,4 +1,28 @@
+import java.util.*;
 
 public class Test {
-	
+
+	int num;
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	public static void main(String[] args) {
+		Test test = new Test();
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("숫자를 입력하세요 :");
+		test.setNum(scan.nextInt());
+		
+		if (test.getNum() % 2 == 0){
+			System.out.println("짝수입니다.");
+		} else {
+			System.out.println("홀수입니다.");
+		}
+	}
 }
